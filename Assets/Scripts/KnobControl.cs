9510fcdc64aadb1f3e1dd960712ac84a38a8f013
Amy Stevenson <ASTEVE204@caledonian.ac.uk>
2018-01-27 04:07:48 +0000

    Created simple rotation script for control knobs Added script for start/stop simulation

--- Assets/Scripts/KnobControl.cs
+++ Assets/Scripts/KnobControl.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class KnobControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        //
-		
+    //Game Object for emitters
+    public GameObject laser;
+
+    //Rotation of control knob and emitter
+    public Quaternion rot;
+
+    //Use this for initialization
+    void Start () {
+        rot = transform.rotation;
 	}
 	
-	// Update is called once per frame
+	//Update is called once per frame
 	void Update () {
-		
-	}
+        //Rotations for knobs and emitters are updated
+        rot = transform.rotation;
+        laser.transform.rotation = rot;
+    }
+
+    //For turning the knobs
+    void OnMouseDown () {
+        //Knob, when tapped, rotates by 22.5° around y-axis
+        GameObject knob = this.gameObject;
+        Vector3 clickedEuler = knob.transform.eulerAngles;
+        if (clickedEuler.y < 157.5f) {
+            knob.transform.eulerAngles = new Vector3(0, clickedEuler.y + 22.5f, 0);
+        } else {
+            knob.transform.eulerAngles = new Vector3(0, 22.5f, 0);
+        }
+        rot = knob.transform.rotation;
+    }
 }
