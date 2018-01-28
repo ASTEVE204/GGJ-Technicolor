4874422e0702889681f59ca7c1cbc019ba4b67eb
ASTEVE204 <asteve204@caledonian.ac.uk>
2018-01-28 14:37:11 +0000

    complete but can't remember how to scene change

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelScript : MonoBehaviour {
+	
+	public GameObject snow;
+	public PrismSpawn[] prismSpawns;
+	bool levelComplete = false;
+	bool[] prismsComplete;
+	bool[] finisheds;
 
 // Use this for initialization
 void Start () {
 	audio.Play();
+	finisheds = new bool[prismSpawns.length];
+	foreach (bool finished in finisheds) {
+		finished = true;
+	}
 }
 	
 // Update is called once per frame
@@ -17,5 +27,14 @@ void Start () {
             	GameData gameDataScript = gameData.GetComponent<GameData>();
             	gameDataScript.lastLevel = "";
         }
+	for (int i = 0; i < prismSpawns.length; i++) {
+		if (prismSpawns[i].levelComplete == true) {
+			prismsComplete[i] = true;
+		}
+	}
+	levelComplete = ArrayEquals(prismSpawns, finisheds);
+	if (levelComplete == true) {
+		snow.setActive(true);
+	}
     }
 }
